9a697575bf3be69a4742800222aacf755ae7272e
Mahmoud-Ehab <mo.ehab150@gmail.com>
2023-08-15 16:50:43 +0300

    QueryGenerator is now exported in index.ts

--- src/backend/Database/Storage/index.ts
+++ src/backend/Database/Storage/index.ts
@@ -1,7 +1,8 @@
 import { QueryHandler } from "./Query/QueryHandler";
 import { QueryConfig } from "./Query/QueryConfig";
 import { QueryResult } from "./Query/QueryResult";
+import { QueryGenerator } from "./Query/QueryGenerator";
 
 export {
-  QueryHandler, QueryConfig, QueryResult
+  QueryHandler, QueryConfig, QueryResult, QueryGenerator
 }
